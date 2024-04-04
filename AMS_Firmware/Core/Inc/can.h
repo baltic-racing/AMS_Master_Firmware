029cc6f4988374ca02a17c68b38539e944dbe864
Valborian <62020204+Valborian@users.noreply.github.com>
2024-04-04 17:30:05 +0200

    created can.c und can.h
    Maximus wollte TY 24 branch haben, weil er schlecht geschlafen hat

---
+++ AMS_Firmware/Core/Inc/can.h
@@ -0,0 +1,13 @@
+/*
+ * can.h
+ *
+ *  Created on: Apr 4, 2024
+ *      Author: Baltic Racing HOST
+ */
+
+#ifndef INC_CAN_H_
+#define INC_CAN_H_
+
+
+
+#endif /* INC_CAN_H_ */
---
+++ AMS_Firmware/Core/Src/can.c
@@ -0,0 +1,10 @@
+/*
+ * can.c
+ *
+ *  Created on: Apr 4, 2024
+ *      Author: Baltic Racing HOST
+ */
+
+#include "can.h"
+
+
